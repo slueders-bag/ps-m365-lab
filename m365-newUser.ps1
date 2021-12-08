23a806832b6045a71d3d950e57c0e49dc8ec87b8
slueders-bag <slueders@baggenstos.ch>
2021-12-08 17:10:57 +0100

    labs done

--- m365-newUser.ps1
+++ m365-newUser.ps1
@@ -1,4 +1,44 @@
+<#
+.SYNOPSIS
+  <new user from csv>
 
+.DESCRIPTION
+  <Brief description of script>
+
+.INPUTS
+  <Inputs if any, otherwise state None>
+
+.OUTPUTS
+  <Outputs if any, otherwise state None - Example: Log file stored in C:\Windows\Temp\<name>.log>
+
+.NOTES
+  Version:        <1.0>
+  Author:         <SLU>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+import-module AzureAD
+Connect-AzureAD
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+# Declare Variables in this area
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "Password1+"
+
+[string]$report = @()
+$company = "SLU Corp"
+$report += "<h1>New User Report</h1>"
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+# Use this area for writing functions
 function New-RandomUser {
     <#
         .SYNOPSIS
@@ -66,5 +106,56 @@ function New-RandomUser {
     Invoke-RestMethod -Uri $rootUrl
 }
 
-$usr = New-RandomUser -Amount 1 -Nationality ch,de -Format csv
-$us = New-RandomUser -Amount 10 -Nationality ch,de -Format json
+function Send-ReportbyMail ($credentials, $To, $From, $subject, $body)
+{
+    Send-MailMessage -To $To -from $From -Subject $subject -Body $body -BodyAsHtml -smtpserver smtp.office365.com -usessl -Credential $credentials -Port 587 
+    Write-host "Report sent to: $to" -ForegroundColor Green
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#CSV
+$usr = New-RandomUser -Amount 10 -Nationality ch,de -Format csv
+
+#JSON
+#$us = New-RandomUser -Amount 10 -Nationality ch,de -Format json
+
+#convert to PS Object 
+$userObj = ConvertFrom-Csv -InputObject $usr
+
+#foreach objects
+$userObj | ForEach-Object{
+    
+    $upn = $_.email -replace "example.com","M365x682019.onmicrosoft.com"
+
+    $newUser = New-AzureADUser -AccountEnabled $true `
+                    -Country $_.'location.country'                        `
+                    -City $_.'location.city'                       `
+                    -CompanyName $Company                        `
+                    -GivenName $_.'name.first'                       `
+                    -DisplayName ($_.'name.first' + " " + $_.'name.last')                         `
+                    -Surname $_.'name.last'                       `
+                    -PostalCode $_.'location.postcode'                       `
+                    -UserPrincipalName $upn                       `
+                    -Mobile $_.'cell'                      `
+                    -TelephoneNumber $_.'phone'                       `
+                    -UsageLocation $_.nat   `
+                    -State $_.'location.state'                   `
+                    -StreetAddress ($_.'location.street.name' + " " + $_.'location.street.number')                       `
+                    -MailNickName  ($_.email -replace "@example.com","")                     `
+                    -PasswordProfile $PasswordProfile `
+                    -Verbose        
+                                   `
+    $pic = Invoke-WebRequest -Uri $user.'picture.large'
+    Set-AzureADUserThumbnailPhoto -ObjectId $newUser.objectid -FileStream $pic.RawContentStream -Verbose
+
+    # show pic
+    #Get-AzureADUserThumbnailPhoto -ObjectId $newUser.objectid -View $true
+    
+    # set dates
+    #Update-MgUser -UserId $newUser.objectid -Birthday $_.'dob.date' -HireDate $_.'registered.date' -EmployeeHireDate $_.'registered.date'
+    $picUrl = '<img src="'+$user.'picture.large'+'">'
+    $report += $newUser | Select-Object Displayname,UserPrincipalName,MailNickName,usagelocation,@{name="Pic";expression='<img src="https://randomuser.me/api/portraits/men/81.jpg">'} | ConvertTo-Html -Fragment -As Table | Out-String
+}
+
+Send-ReportbyMail -credentials $cred -To hans.dampf@baggenstos.ch -From admin@M365x682019.onmicrosoft.com -subject ("New User Report " + (get-date -format d)) -body $report
