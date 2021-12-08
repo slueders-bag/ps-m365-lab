f665a56cf36bd89f5cb85c8d332de3a47c133a29
slueders-bag <slueders@baggenstos.ch>
2021-12-08 10:59:42 +0100

    lab1

---
+++ m365-lab.ps1
@@ -0,0 +1,12 @@
+
+import-module azuread
+
+# azure ad
+Connect-AzureAD
+
+# count all user accounts
+Get-Command -Module azuread -noun azureaduser
+Get-AzureADUser -All $true | Measure-Object
+$all = Get-AzureADUser -All $true
+$all | Get-Member
+$all.city
