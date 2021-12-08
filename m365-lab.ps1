d88d64f4bc0227b35469461a6ae462e3d6b93bcb
slueders-bag <slueders@baggenstos.ch>
2021-12-08 12:02:36 +0100

    labs

--- m365-lab.ps1
+++ m365-lab.ps1
@@ -12,3 +12,28 @@ $all | Get-Member
 $all.city
 
 # sort / group department
+Get-AzureADUser -All $true | Select-Object department -Unique
+$all.department | Select-Object -Unique
+$all | Group-Object department 
+
+# user without department
+$all | Group-Object department | select -ExpandProperty Group -First 1
+$all | sort department | ft -AutoSize department,displayname 
+$all | Where-Object{$PSItem.department -notlike ""}
+$all | Where-Object{$PSItem.department -ne $null -and $_.country -like "Unit*"}
+
+# Filter parameter
+Get-AzureADUser -Filter "country eq 'United States'"
+Get-AzureADUser -Filter "startswith(country,'unit')"
+
+$alias = $all.userprincipalname -replace ("@M365x682019.OnMicrosoft.com","")
+SMTP:vorname.nachname@contoso.com
+
+$all | ForEach-Object{
+        $pre = "SMTP:" + $_.displayname -replace (" ",".")
+        $pre + "@" +(Get-AzureADDomain).Name
+    }
+
+
+
+
